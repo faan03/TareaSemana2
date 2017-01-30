2ff6b835ed222b245f696ddf617c9f18171bee15
faan03 <faan03@gmail.com>
2017-01-29 22:15:49 -0500

    Initial commit

---
+++ app/src/main/java/com/faan/tareasemana2/contactoActivity.java
@@ -0,0 +1,13 @@
+package com.faan.tareasemana2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class contactoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacto);
+    }
+}
